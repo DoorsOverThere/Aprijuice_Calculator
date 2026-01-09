6fa428f75242f85d412223b6a895e0df6717f07b
adwyer02 <aidandyr@gmail.com>
2026-01-09 05:47:03 -0600

    fixes

--- js/media.js
+++ js/media.js
@@ -1,12 +1,16 @@
 window.addEventListener('resize', adjustForMedia);
 adjustForMedia();
 
+const leftContainer = document.getElementById('leftContainer');
+const mainContainer = document.getElementById('mainContainer');
 function adjustForMedia() {
     if (window.matchMedia("(min-width: 1300px)").matches) {
-        document.getElementById('leftContainer').prepend(document.getElementById('campfirePot'));
-        document.getElementById('leftContainer').prepend(document.getElementById('title'));
+        leftContainer.prepend(document.getElementById('campfirePot'));
+        leftContainer.prepend(document.getElementById('titleDescription'));
+        leftContainer.prepend(document.getElementById('title'));
     } else {
-        document.getElementById('mainContainer').prepend(document.getElementById('campfirePot'));
-        document.getElementById('mainContainer').prepend(document.getElementById('title'));
+        mainContainer.prepend(document.getElementById('campfirePot'));
+        mainContainer.prepend(document.getElementById('titleDescription'));
+        mainContainer.prepend(document.getElementById('title'));
     }
 }
